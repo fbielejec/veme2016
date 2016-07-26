b7a0d19f3335353b80ec3a0c6c67a944add19c4c
fbielejec <fbielejec@gmail.com>
2016-07-26 21:06:52 +0200

    commiting latest changes which I did not commit during the course

--- Advanced_Evolutionary_Models_and_Likelihood_Methods/plots/plots.r
+++ Advanced_Evolutionary_Models_and_Likelihood_Methods/plots/plots.r
@@ -12,7 +12,7 @@ require(reshape2)
 paths = 1
 N = 50
 
-rate = 10
+rate = 0.1
 
 colorBeta <- rgb(252, 141, 98, maxColorValue = 255, alpha = 255)  
 colorAlpha <- rgb(141, 160, 203, maxColorValue = 255, alpha = 255)  
@@ -93,9 +93,9 @@ print(p)
 #########################
 #---JC69 CTMC EXAMPLE---#
 #########################
-JC69 <- function (lambda, t) {
-  p0 = 1/4 + 3/4 * exp(-4*lambda*t)
-  p1 = 1/4 - 1/4 * exp(-4*lambda*t)
+JC69 <- function (theta, t) {
+  p0 = 1/4 + 3/4 * exp(-4*theta*t)
+  p1 = 1/4 - 1/4 * exp(-4*theta*t)
   
   trans_prob = matrix(c(p0, p1, p1, p1,
                         p1, p0, p1, p1,
@@ -116,17 +116,17 @@ data <- data.frame(
     G = rep(NA, N)
     )
 
-set.seed(123)
+# set.seed(123)
 r    <- runif(4)
 vect <- r / sum(r)
 
 
 data[1, ] <- vect
-lambda    <- 2
+theta    <- 2
 
 for (i in 2 : N) {
    probs = as.matrix(data[i - 1, ], nrow = 1, ncol = 4)
-   data[i, ] <- probs %*% JC69(lambda, t = times[i])   
+   data[i, ] <- probs %*% JC69(theta, t = times[i])   
 }
 
 data_melt <- melt(cbind(t = times, data), id.vars = "t") 
@@ -148,5 +148,67 @@ theme <- theme(
 p <- p + theme
 print(p)
 
-# library(expm)
-# c(0,1,0,0) %*% (JC69(lambda = lambda, t=100))
+
+###################################
+#---BINOMIAL LIKELIHOOD EXAMPLE---#
+###################################
+myBlue = rgb(r = 86, g = 110, b = 126, maxColorValue = 250)
+
+like <- function(theta, k = 8, n = 10) {
+  return(dbinom(k, n, theta))
+}
+
+# 900 x 600
+theta <- seq(from = 0, to = 1, by = 0.01)
+plot(theta, like(theta), type = "l", col = myBlue, lwd = 3)
+
+
+
+
+points(0.8, like(0.8), pch = 19, col = myBlue, cex = 1.5)
+
+
+######################
+#---NEWTON RAPHSON---#
+######################
+myBlue = rgb(r = 86, g = 110, b = 126, maxColorValue = 250)
+
+f <- function(x) {
+  f = exp(x)/(1 + exp(x))^2
+  fx = exp(x)/(1 + exp(x))^2 - 2*exp(2*x)/(1 + exp(x))^3
+  fxx = 6 * exp(x)/(1 + exp(x))^4 - 6*exp(2*x)/(1 + exp(x))^3 + exp(x)/(1+exp(x))^2
+  return( data.frame(f = f, fx = fx, fxx = fxx ) )
+}
+
+x = seq(-10, 10, length.out = 100)
+plot(x, f(x)[,1], type = "l", col = myBlue, lwd = 3)
+
+newtonRaphson <- function(f, x0, tol = 1/10^9, maxIter = 100) {
+  
+  x  = x0
+  fx = f(x)
+  i  = 0
+  
+  cat(paste(c("i", "x", "f(x)", "\n"), sep = "\t"))
+  while ( (abs(fx[, 2]) > tol) ) {
+    
+    if(i < maxIter) {
+      
+      x  = x - fx[, 2] / fx[, 3]
+      fx = f(x)
+      
+      points(x, fx[,1], pch = 19, col = myBlue, cex = 1.5)
+      cat(paste(c(i, x, fx[,1], "\n"), sep = "\t"))
+      Sys.sleep(0.5)  
+      
+      i  = i + 1
+      
+    } else {
+      break;
+    }#END: maxIter check
+  }#END: tolerance loop
+  
+  return(x)
+}
+
+x.star <- newtonRaphson(f, 5)
--- ML_methods_and_hypothesis_testing_in_molecular_phylogenetics/plots/plots.r
+++ ML_methods_and_hypothesis_testing_in_molecular_phylogenetics/plots/plots.r
@@ -5,10 +5,70 @@ require(ggplot2)
 require(reshape2)
 
 
+#########################
+#---JC69 CTMC EXAMPLE---#
+#########################
+JC69 <- function (theta, t) {
+  p0 = 1/4 + 3/4 * exp(-4*theta*t)
+  p1 = 1/4 - 1/4 * exp(-4*theta*t)
+  
+  trans_prob = matrix(c(p0, p1, p1, p1,
+                        p1, p0, p1, p1,
+                        p1, p1, p0, p1,
+                        p1, p1, p1, p0
+  ), nrow = 4, ncol = 4, byrow = T)
+  
+  colnames(trans_prob) <- rownames(trans_prob) <- c("T", "C", "A", "G")
+  return(trans_prob)
+}
+
+N = 50
+times = seq(0, 0.1, length.out = N)
+data <- data.frame(
+  T = rep(NA, N),
+  C = rep(NA, N),
+  A = rep(NA, N),
+  G = rep(NA, N)
+)
+
+# set.seed(123)
+r    <- runif(4)
+vect <- r / sum(r)
+
+
+data[1, ] <- vect
+theta    <- 2
+
+for (i in 2 : N) {
+  probs = as.matrix(data[i - 1, ], nrow = 1, ncol = 4)
+  data[i, ] <- probs %*% JC69(theta, t = times[i])   
+}
+
+data_melt <- melt(cbind(t = times, data), id.vars = "t") 
+
+p <- ggplot(data_melt)
+p <- p + geom_line(aes(x = t, y = value, color = variable))    
+p <- p + xlab("Time") + ylab("Probability")
+theme <- theme(
+  axis.title = element_text(colour = "black"),
+  axis.text = element_text(colour = "black"),
+  axis.line = element_line(colour = "black"),
+  axis.ticks = element_line(colour = "black"),
+  panel.background = element_rect(size = 1, fill = "white", colour = NA),
+  plot.background = element_rect(colour = NA),
+  panel.border = element_blank(),
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank()
+)
+p <- p + theme
+print(p)
+
 ###################################
 #---BINOMIAL LIKELIHOOD EXAMPLE---#
 ###################################
 myBlue = rgb(r = 86, g = 110, b = 126, maxColorValue = 250)
+
+
 
 like <- function(theta, k = 8, n = 10) {
   return(dbinom(k, n, theta))
@@ -18,57 +78,9 @@ like <- function(theta, k = 8, n = 10) {
 theta <- seq(from = 0, to = 1, by = 0.01)
 plot(theta, like(theta), type = "l", col = myBlue, lwd = 3)
 
+
 points(0.8, like(0.8), pch = 19, col = myBlue, cex = 1.5)
 
 
-######################
-#---NEWTON RAPHSON---#
-######################
-myBlue = rgb(r = 86, g = 110, b = 126, maxColorValue = 250)
-
-f <- function(x) {
-  f = exp(x)/(1 + exp(x))^2
-  fx = exp(x)/(1 + exp(x))^2 - 2*exp(2*x)/(1 + exp(x))^3
-  fxx = 6 * exp(x)/(1 + exp(x))^4 - 6*exp(2*x)/(1 + exp(x))^3 + exp(x)/(1+exp(x))^2
-  return( data.frame(f = f, fx = fx, fxx = fxx ) )
-}
-
-x = seq(-10, 10, length.out = 100)
-plot(x, f(x)[,1], type = "l", col = myBlue, lwd = 3)
-
-newtonRaphson <- function(f, x0, tol = 1/10^9, maxIter = 100) {
-
-      x  = x0
-      fx = f(x)
-      i  = 0
-      
-      cat(paste(c("i", "x", "f(x)", "\n"), sep = "\t"))
-      while ( (abs(fx[, 2]) > tol) ) {
-      
-      if(i < maxIter) {
-      
-             x  = x - fx[, 2] / fx[, 3]
-             fx = f(x)
-             
-             points(x, fx[,1], pch = 19, col = myBlue, cex = 1.5)
-             cat(paste(c(i, x, fx[,1], "\n"), sep = "\t"))
-             Sys.sleep(0.5)  
-             
-             i  = i + 1
-             
-      } else {
-      break;
-      }#END: maxIter check
-  }#END: tolerance loop
-  
-  return(x)
-}
-
-x.star <- newtonRaphson(f, 5)
-# f(x.star)
-
-
-
-
-
+# fair coin?
 
